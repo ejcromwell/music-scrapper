e5a825ab0c1d7419f4e5d08e54daeb2457dbb26f
Elliot Laptop <elliot.cromwell@thecroc.com>
2021-11-18 23:32:54 +0000

    Added table row and table header components for web scrape results table.

---
+++ Components/WebResults/Table/TableHeader.jsx
@@ -0,0 +1,19 @@
+function TableHeader() {
+  return (
+    <thead>
+      <tr>
+        <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
+          ID
+        </th>
+        <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+          Title
+        </th>
+        <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+          Artist
+        </th>
+      </tr>
+    </thead>
+  );
+}
+
+export default TableHeader;
---
+++ Components/WebResults/Table/TableRow.jsx
@@ -0,0 +1,23 @@
+function TableRow({row, index}) {
+  return (
+    <tr key={index}>
+      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+        <div className="flex items-center">
+          <div>
+            <div className="text-sm leading-5 text-gray-800">#{index + 1}</div>
+          </div>
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+        <div className="text-sm leading-5 text-blue-900">
+          {row.title}
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
+        {row.artist}
+      </td>
+    </tr>
+  );
+}
+
+export default TableRow;
