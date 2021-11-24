83b63c11af72eface48112758cf726224217b285
Elliot Laptop <elliot.cromwell@thecroc.com>
2021-11-24 19:33:20 +0000

    Updated table row and header with fields for id and status

--- Components/WebResults/Table/TableHeader.jsx
+++ Components/WebResults/Table/TableHeader.jsx
@@ -3,6 +3,8 @@ function TableHeader() {
     <thead>
       <tr>
         <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
+        </th>
+        <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
           ID
         </th>
         <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
@@ -10,6 +12,9 @@ function TableHeader() {
         </th>
         <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
           Artist
+        </th>
+        <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+          Status
         </th>
       </tr>
     </thead>
--- Components/WebResults/Table/TableRow.jsx
+++ Components/WebResults/Table/TableRow.jsx
@@ -2,9 +2,16 @@ function TableRow({row, index}) {
   return (
     <tr key={index}>
       <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+        <div className="text-sm leading-5 text-blue-900">
+          // Check
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
         <div className="flex items-center">
           <div>
-            <div className="text-sm leading-5 text-gray-800">#{index + 1}</div>
+            <div className="text-sm leading-5 text-gray-800">
+              {row.id}
+            </div>
           </div>
         </div>
       </td>
@@ -15,6 +22,9 @@ function TableRow({row, index}) {
       </td>
       <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
         {row.artist}
+      </td>
+      <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
+        status
       </td>
     </tr>
   );
