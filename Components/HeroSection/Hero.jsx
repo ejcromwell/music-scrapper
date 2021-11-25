8fd3692c1829fdf71c999b47ba41cae5b7cf6a5f
Elliot Laptop <elliot.cromwell@thecroc.com>
2021-11-25 18:15:37 +0000

    Moved functionality to helper lib file.
    Added useEffect to this parent to remove from lower down components to pass values from higher in in prop chain.

--- Components/HeroSection/Hero.jsx
+++ Components/HeroSection/Hero.jsx
@@ -1,55 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import {getTracks} from '../../lib/tracks';
 
 import WebsiteSelection from './WebsiteSelection.jsx'
 import WebResults from '../WebResults/WebResults.jsx';
 
-
-function createEndpoint(inputs) {
-  if (inputs.url === undefined || inputs.location === undefined) {
-    return false;
-  }
-
-  let url = "/api/scraper?";
-  url += "location=" + encodeURIComponent(inputs.location);
-  url += "&url=" + encodeURIComponent(inputs.url);
-
-  return url;
-}
-
-/**
- * Get the tracks array from the scraper API endpoint
- *
- * @param {object} inputs Inputs values from the form
- * @returns object With status code and data
- */
-async function getTracks(inputs) {
-
-  const url = createEndpoint(inputs);
-  if (url === false) {
-    return { status: 500, data: "" };
-  }
-
-  try {
-    const response = await fetch(url);
-    if (response.status === 404) {
-      return { status: 404, data: "" };
-    }
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    // grab track list from internal API
-    const data = await response.json();
-    // set data in localStorage for later access
-    localStorage.setItem('trackList', JSON.stringify(data));
-    return {status: 200, data: data};
-
-  } catch (error) {
-    console.log("error with getUrl: ", error);
-    return { status: 500, data: '' };
-  }
-
-}
 
 function Hero() {
 
@@ -65,6 +19,13 @@ function Hero() {
     }
     setError(true);
   }
+
+  useEffect(() => {
+    const trackListJSON = localStorage.getItem('trackList');
+    if (trackListJSON && trackListJSON !== "") {
+      setTracks(JSON.parse(trackListJSON));
+    }
+  }, []);
 
   return (
     <div className="bg-gray-800">
