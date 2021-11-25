68bde11ba879b19098ee84106957be477f8b29e3
Elliot Laptop <elliot.cromwell@thecroc.com>
2021-11-25 18:19:32 +0000

    Update class validation.
    Added checkbox functionality.

--- Components/WebResults/Table/TableRow.jsx
+++ Components/WebResults/Table/TableRow.jsx
@@ -1,50 +1,83 @@
-function TableRow({row, index}) {
+import { useEffect, useState } from "react";
 
-  let ariaStatus, textColour;
 
-  if (row.status === "Added") {
-    ariaStatus = " bg-green-200";
-    textColour = " text-green-900 ";
-  }
+/**
+ * Validate Tailwind class needed for bg colour depending on status value.
+ *
+ * @param {String} status Status value
+ * @returns String
+ */
+function bgColour(status) {
+  if (status === "Added") return " bg-green-200";
+  if (status === "Duplicate") return " bg-yellow-200";
+  if (status === "Not Found") return " bg-red-200";
+}
 
-  if (row.status === "Duplicate") {
-    ariaStatus = " bg-orange-200";
-    textColour = " text-orange-900";
-  }
 
-  if (row.status === "Not Found") {
-    ariaStatus = " bg-red-200";
-    textColour = " text-red-900";
-  }
+/**
+ * Validate Tailwind class needed for text colour depending on status value.
+ *
+ * @param {String} status Status value
+ * @returns String
+ */
+function textColourValue(status) {
+  if (status === "Added") return " text-green-900";
+  if (status === "Duplicate") return " text-yellow-900";
+  if (status === "Not Found") return " text-red-900";
+}
+
+
+function TableRow({row, handleChange, isChecked}) {
+
+  const [checkedValue, setCheckedValue] = useState(isChecked);
+
+  const ariaStatus = bgColour(row.status);
+  const textColour = textColourValue(row.status);
+
+
+  useEffect(() => {
+    setCheckedValue(isChecked); // Initially set the input checked status
+  }, [isChecked]);
 
   return (
-    <tr key={index}>
+    <tr>
       <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
         <div className="text-sm leading-5 text-blue-900">
-          // Check
+          <input
+            key={row.id}
+            id={`track-checkbox-${row.id}`}
+            name={`track-checkbox-${row.id}`}
+            type="checkbox"
+            checked={checkedValue}
+            onChange={ () => {
+              handleChange(row.id);
+              setCheckedValue(!checkedValue);
+            } }
+          />
         </div>
       </td>
       <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
         <div className="flex items-center">
           <div>
-            <div className="text-sm leading-5 text-gray-800">
-              {row.id}
-            </div>
+            <div className="text-sm leading-5 text-gray-800">{row.id}</div>
           </div>
         </div>
       </td>
       <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-        <div className="text-sm leading-5 text-blue-900">
-          {row.title}
-        </div>
+        <div className="text-sm leading-5 text-blue-900">{row.title}</div>
       </td>
       <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
         {row.artist}
       </td>
       <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
-        <span className={`relative inline-block px-3 py-1 font-semibold leading-tight ${textColour}`}>
-          <span aria-hidden className={`absolute inset-0 opacity-50 rounded-full ${ariaStatus}`}></span>
-          <span className="relative text-xs">{status}</span>
+        <span
+          className={`relative inline-block px-3 py-1 font-semibold leading-tight ${textColour}`}
+        >
+          <span
+            aria-hidden
+            className={`absolute inset-0 opacity-50 rounded-full ${ariaStatus}`}
+          ></span>
+          <span className="relative text-xs">{row.status}</span>
         </span>
       </td>
     </tr>
